6e3e7cd28832394c0c0b7ea9ae9be8223680bb76
nishit-95 <nishitvataliya003>
2024-04-19 22:56:19 +0530

    LongestCommonPrefix added

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,31 @@
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] S) {
+        if (S == null || S.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < S[0].length(); i++) {
+            char c = S[0].charAt(i);
+            for (int j = 1; j < S.length; j++) {
+                if (i == S[j].length() || S[j].charAt(i) != c) {
+                    return S[0].substring(0, i);
+                }
+            }
+        }
+        return S[0];
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix solution = new LongestCommonPrefix();
+        
+        // Test cases
+        String[] strings1 = {"flower", "flow", "flight"};
+        String[] strings2 = {"dog", "racecar", "car"};
+        String[] strings3 = {"apple", "applebee", "app"};
+        
+        // Output the result of each test case
+        System.out.println("Longest common prefix for strings1: " + solution.longestCommonPrefix(strings1));
+        System.out.println("Longest common prefix for strings2: " + solution.longestCommonPrefix(strings2));
+        System.out.println("Longest common prefix for strings3: " + solution.longestCommonPrefix(strings3));
+    }
+}
+
