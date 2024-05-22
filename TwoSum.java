d39f6d524003f2cf05d79a5dd86bdf5f237172a5
nishit-95 <nishitvataliya003>
2024-05-22 19:13:57 +0530

    using hashmap

--- TwoSum.java
+++ TwoSum.java
@@ -1,24 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
-            }
-        }
-        return new int[0];
-    }
+        Map<Integer, Integer> map = new HashMap<>();
 
-    public static void main(String[] args) {
-        TwoSum solution = new TwoSum();
-        int[] nums = { 2, 7, 11, 15 };
-        int target = 9;
-        int[] result = solution.twoSum(nums, target);
-        if (result.length == 2) {
-            System.out.println("Indices: " + result[0] + ", " + result[1]);
-        } else {
-            System.out.println("No solution found.");
+        for(int i=0; i<nums.length; i++){
+            int com = target - nums[i];
+
+            if(map.containsKey(com)){
+                return new int[]{map.get(com),i};
+            }
+            map.put(nums[i],i);
         }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
